f0b2c3c8c65d21e5a4bbafd3847f3336a757d016
hsing_liu <hsing_liu@mail.chungyo.net>
2021-01-18 16:58:45 +0800

    [Changed]refactor test

--- business/accounting_test.go
+++ business/accounting_test.go
@@ -1,11 +1,11 @@
 package business
 
 import (
+	"log"
 	"testing"
 	"time"
 
 	"bou.ke/monkey"
-
 	"github.com/stretchr/testify/suite"
 )
 
@@ -24,27 +24,53 @@ func (at *AccountingSuite) SetupTest() {
 }
 
 func (at *AccountingSuite) Test_no_budget() {
-	start, _ := time.Parse("2006-01-02", "2021-04-01")
-	end, _ := time.Parse("2006-01-02", "2021-04-01")
+
+	start, end := at.setStartEnd("2021-04-01", "2021-04-01")
 
 	at.totalShouldBe(start, end, 0)
 }
 
 func (at *AccountingSuite) Test_period_inside_budget_month() {
-	start, _ := time.Parse("2006-01-02", "2021-04-01")
-	end, _ := time.Parse("2006-01-02", "2021-04-30")
 
 	// mock
-	monkey.Patch(GetBudgets, func() []Budget {
-		budgets := []Budget{
-			{"202004", 3000},
-		}
-		return budgets
-	})
+	mock := at.mockGetBudgets(map[string]float64{"202104": 3000})
+	defer mock.Unpatch()
 
+	start, end := at.setStartEnd("2021-04-01", "2021-04-30")
 	at.totalShouldBe(start, end, 3000)
 }
 
 func (at *AccountingSuite) totalShouldBe(start, end time.Time, expected float64) {
 	at.Equal(expected, at.Accounting.GetTotal(start, end))
+}
+
+func (at *AccountingSuite) mockGetBudgets(mockData map[string]float64) *monkey.PatchGuard {
+
+	mock := monkey.Patch(GetBudgets, func() (budgets []Budget) {
+		for yearMonth, amount := range mockData {
+			budgets = append(budgets, Budget{yearMonth, amount})
+		}
+		return budgets
+	})
+
+	return mock
+}
+
+func (at *AccountingSuite) setStartEnd(startStr, endStr string) (
+	start, end time.Time,
+) {
+
+	var err error
+	start, err = time.Parse("2006-01-02", startStr)
+	if err != nil {
+		log.Println("[ 設定初始化開始時間失敗 ] GOT: ", startStr)
+		return
+	}
+	end, err = time.Parse("2006-01-02", endStr)
+	if err != nil {
+		log.Println("[ 設定初始化結束時間失敗 ] GOT: ", endStr)
+		return
+	}
+
+	return
 }
