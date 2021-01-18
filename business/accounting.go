be47cbbfcb5d0097af838f5ee11544310534ea73
hsing_liu <hsing_liu@mail.chungyo.net>
2021-01-18 18:08:23 +0800

    [Changed]merge check overlapping day

--- business/accounting.go
+++ business/accounting.go
@@ -18,23 +18,8 @@ func (a *Accounting) GetTotal(start, end time.Time) (total float64) {
 
 	budgets := GetBudgets()
 	for _, budget := range budgets {
-
-		// 判斷結束時間有沒有在預算最後一天之後
-		if end.After(budget.LastDay()) {
-			return 0
-		}
-
-		// 判斷開始時間有沒有在預算第一天之後
-		if start.Before(budget.FirstDay()) {
-			return 0
-		}
-
-		return a.periodDiff(start, end) * budget.DailyAmount()
+		return budget.overlappingAmount(start, end)
 	}
 
 	return 0
-}
-
-func (a *Accounting) periodDiff(start, end time.Time) float64 {
-	return end.Sub(start).Hours()/24 + 1
 }
--- business/budget.go
+++ business/budget.go
@@ -2,6 +2,7 @@ package business
 
 import (
 	"log"
+	"strconv"
 	"time"
 )
 
@@ -22,11 +23,12 @@ func GetBudgets() (budgets []Budget) {
 // DailyAmount 取得每日金額
 func (b *Budget) DailyAmount() float64 {
 
-	return b.Amount / 30
+	return b.Amount / float64(b.totalDay())
 }
 
 // FirstDay 預算第一天
 func (b *Budget) FirstDay() (firstDay time.Time) {
+
 	var err error
 	firstDay, err = time.Parse("20060102", b.YearMonth+"01")
 	if err != nil {
@@ -41,11 +43,40 @@ func (b *Budget) FirstDay() (firstDay time.Time) {
 func (b *Budget) LastDay() (lastDay time.Time) {
 
 	var err error
-	lastDay, err = time.Parse("20060102", b.YearMonth+"30")
+	lastDay, err = time.Parse(
+		"20060102", b.YearMonth+strconv.Itoa(b.totalDay()),
+	)
 	if err != nil {
 		log.Println("[ 判斷預算最後一天轉型失敗 ] Err:", err)
 		return
 	}
 
 	return
+}
+
+// totalDay 預算總共有幾天
+func (b *Budget) totalDay() int {
+
+	date, err := time.Parse("200601", b.YearMonth)
+	if err != nil {
+		return 0
+	}
+
+	d := time.Date(date.Year(), date.Month()+1, 0, 0, 0, 0, 0, time.UTC)
+
+	return d.Day()
+}
+
+func (b *Budget) overlappingAmount(start, end time.Time) float64 {
+
+	// 判斷結束時間有沒有在預算最後一天之後
+	if end.After(b.LastDay()) || start.Before(b.FirstDay()) {
+		return 0
+	}
+
+	return b.periodDiff(start, end) * b.DailyAmount()
+}
+
+func (b *Budget) periodDiff(start, end time.Time) float64 {
+	return end.Sub(start).Hours()/24 + 1
 }
