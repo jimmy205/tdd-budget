3681695bf36bcc46af7ccb130b7f2e23b9acecac
hsing_liu <hsing_liu@mail.chungyo.net>
2021-01-18 16:25:04 +0800

    [Added]fail period inside month test && rename test function

--- business/accounting_test.go
+++ business/accounting_test.go
@@ -21,11 +21,18 @@ func (at *AccountingSuite) SetupTest() {
 	at.Accounting = NewAccounting()
 }
 
-func (at *AccountingSuite) Test_GetTotal() {
+func (at *AccountingSuite) Test_no_budget() {
 	start, _ := time.Parse("2006-01-02", "2021-04-01")
 	end, _ := time.Parse("2006-01-02", "2021-04-01")
 
 	at.totalShouldBe(start, end, 0)
+}
+
+func (at *AccountingSuite) Test_period_inside_budget_month() {
+	start, _ := time.Parse("2006-01-02", "2021-04-01")
+	end, _ := time.Parse("2006-01-02", "2021-04-30")
+
+	at.totalShouldBe(start, end, 3000)
 }
 
 func (at *AccountingSuite) totalShouldBe(start, end time.Time, expected float64) {
